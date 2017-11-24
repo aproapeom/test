315fc9a59b7abd44635b81de39fcc0aa4fc13ca8
nuclewar <32749736+nuclewar@users.noreply.github.com>
2017-11-24 12:20:41 +0200

    update vineri 12.00

--- js/main.js
+++ js/main.js
@@ -1,9 +1,18 @@
 //global variables ---------------------------------------------------------------------------------------------------------------
 var arr = [];
 var daychecktemplate = function(k, ix, jj){
-    arr[0+jj] =  '<div class="checkKont"><label for="ziua'+k+'">'+k+'</label><input type="checkbox" id="ziua'+k+'" value="ziua'+k+'" name="ziua"/></div>';
-    arr[1+jj] =  '<div class="checkKont"><label for="luna'+k+'">'+k+'</label><input type="checkbox" id="luna'+k+'" value="luna'+k+'" name="luna"/></div>';
-    arr[2+jj] =  '<div class="checkKont"><label for="an'+k+'">'+k+'</label><input type="checkbox" id="an'+k+'" value="an'+k+'" name="an"/></div>';
+    arr[0+jj] =  '<div class="checkKont"><label for="ziua'+k+'">'+k+'</label><input type="radio" id="ziua'+k+'" value="ziua'+k+'" name="ziua"/></div>';
+    arr[1+jj] =  '<div class="checkKont"><label for="luna'+k+'">'+k+'</label><input type="radio" id="luna'+k+'" value="luna'+k+'" name="luna"/></div>';
+    arr[2+jj] =  '<div class="checkKont"><label for="an'+k+'">'+k+'</label><input type="radio" id="an'+k+'" value="an'+k+'" name="an"/></div>';
+    arr[3+jj] =  '<div class="checkKont"><label for="deplasare'+k+'">'+k+'</label><input type="radio" id="deplasare'+k+'" value="deplasare'+k+'" name="deplasare"/></div>';
+    arr[4+jj] =  '<div class="checkKont"><label for="permise'+k+'">'+k+'</label><input type="radio" id="permise'+k+'" value="permise'+k+'" name="permise"/></div>';
+    arr[5+jj] =  '<div class="checkKont"><label for="pregatire'+k+'">'+k+'</label><input type="radio" id="pregatire'+k+'" value="pregatire'+k+'" name="pregatire"/></div>';
+    arr[6+jj] =  '<div class="checkKont"><label for="executie'+k+'">'+k+'</label><input type="radio" id="executie'+k+'" value="executie'+k+'" name="executie"/></div>';
+    arr[7+jj] =  '<div class="checkKont"><label for="inchidere'+k+'">'+k+'</label><input type="radio" id="inchidere'+k+'" value="inchidere'+k+'" name="inchidere"/></div>';
+    arr[8+jj] =  '<div class="checkKont"><label for="altele'+k+'">'+k+'</label><input type="radio" id="altele'+k+'" value="altele'+k+'" name="altele"/></div>';
+
+
+
 
     return arr[ix+jj];
 };
@@ -49,13 +58,18 @@ $(document).ready(function () {
     }
 
 
-
-
     if ($("#pontaj").length) {
 
         appendForLoop(1,32,$("#daysCheck"),0,0);
         appendForLoop(1,13,$("#monthCheck"),1,0);
         appendForLoop(2017,2020,$("#yearCheck"),2,0);
+
+        appendForLoop(1,9,$("#deplasareCheck"),3,0);
+        appendForLoop(1,9,$("#permiseCheck"),4,0);
+        appendForLoop(1,9,$("#pregatireCheck"),5,0);
+        appendForLoop(1,9,$("#executieCheck"),6,0);
+        appendForLoop(1,9,$("#inchidereCheck"),7,0);
+        appendForLoop(1,9,$("#alteleCheck"),8,0);
 
         $('#pontaj').iCheck({
             checkboxClass: 'icheckbox_flat-green',
@@ -65,3 +79,12 @@ $(document).ready(function () {
     }
 
 });
+
+
+window.onload = function () {
+
+    setTimeout(function () {
+        $('#preloader').hide();
+    }, 1000);
+
+};
